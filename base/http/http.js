2cdb309233a8ff012c6ec11f18c67269435b2629
yerundong <414868970@qq.com>
2018-09-26 14:14:35 +0800

    3423

--- base/http/http.js
+++ base/http/http.js
@@ -1,25 +1,55 @@
 const http = require('http');
-// console.log(http);
+const url = require('url');
+const querystring = require('querystring');
+//设置主机名
+const hostName = '127.0.0.1';
+//设置端口
+const port = 8888;
 
 // [http.createServer([options][, requestListener])]
-// 
 // 创造一个服务器实例。
 const server = http.createServer(function (req, res) {
-    if(req.url === '/'){
-        res.writeHead(200, { "Content-Type": "text/html" });
+    // url.parse() 方法会解析一个 URL 字符串并返回一个 URL 对象。
+    const urlInfo = url.parse(req.url, true);
+    // console.log(urlInfo);
+
+    req.setEncoding("utf8");
+    let postData = '';
+    // req.addListener('data', (chunk) => {
+    //     console.log(chunk);
+    // });
+
+    // req.addListener('end', function () {
+    //     res.writeHead(200, {
+    //         'Content-Type': 'text/plain'
+    //     });
+    //     res.write("You've sent the text: " + querystring.parse(postData).text);
+    //     res.end();
+    // });
+
+    if (urlInfo.pathname === '/') {
+        res.setHeader('Content-Type','text/plain');
+        res.setHeader('Access-Control-Allow-Origin',"*")
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.write("Hello nodejs");
         res.write("how ar you");
-        res.end('{a: 1, b: 2}');
-    }else if(req.url === '/home'){
-        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end('{a: 1, b: 2}', ()=>{
+            return
+        });
+    } else if (urlInfo.pathname === '/home') {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
         res.write("Welcome home");
         res.end();
-    }else{
-        res.writeHead(404, { "Content-Type": "text/html" });
+    } else {
+        res.writeHead(404, {
+            "Content-Type": "text/html"
+        });
         res.end("404 error! File not found.");
     }
 });
 
-server.listen(8888, () => {
+server.listen(port, hostName, () => {
     console.log('========== Server is running on port 8888 =============');
 });
